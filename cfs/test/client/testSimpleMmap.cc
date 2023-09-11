8a40f4ec2b235c770154c1e459cb91b19a42b02f
MelodyMa <MelodyMa@localhost>
2023-09-11 14:19:44 -0600

    create a new mmap test on client

---
+++ cfs/test/client/testSimpleMmap.cc
@@ -0,0 +1,10 @@
+#include "testClient_common.h"
+#include <iostream>
+
+// this is a simple program that copy a file to program's memory
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
